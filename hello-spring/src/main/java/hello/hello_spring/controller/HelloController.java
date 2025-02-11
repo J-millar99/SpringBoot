ab65a4f0a06dc5f0b40feab9ebd0359cfae11ab1
J-millar99 <wlwogus33@gmail.com>
2025-02-11 20:52:20 +0900

    스프링 웹 개발 기초

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,40 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
-}
+
+    // Model, View, Controller 방식
+    // 뷰 리졸버에게 html의 렌더링을 부탁
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // API 방식
+    // String Converter, Json Converter 를 사용
+    @GetMapping("hello-string")
+    @ResponseBody // ResponseBody를 사용하면 뷰 리졸버를 사용하지 않음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+} 
